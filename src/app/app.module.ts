b42250b094b21b9dcf213cbe40d472063f726d93
mariemtlili21 <mariem.tlili@esprit.tn>
2023-02-24 20:53:13 +0100

    Template Driven Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { FormsModule } from '@angular/forms';
+import { AddproductComponent } from './addproduct/addproduct.component';
+import { ProductdetailsComponent } from './productdetails/productdetails.component';
+import { FormComponent } from './form/form.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    NavbarComponent,
+    NotfoundComponent,
+    AddproductComponent,
+    ProductdetailsComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,  
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
